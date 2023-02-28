95b3cb8d58ca1507df8a752c4ed0d15e2569090d
ryanformio <ryan@form.io>
2023-02-28 13:07:03 -0600

    initial commit

---
+++ tests/main.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../index.js');
+
+describe('Test the root path', () => {
+
+    test('It should response the GET method', async () => {
+        const response = await request('http://localhost:3005').get('/');
+        // console.log(response)
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('Hello World!');
+    });
+});
